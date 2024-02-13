758ad921380cddc5df6652fb25418fae23e66fc7
Ilya Artamonov <hire@ilya-artamonov.ru>
2024-02-13 16:39:57 +0300

    init

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import { h, reactive, nextTick, ref } from 'vue'
+
+const linkAttrs = reactive<{
+  link: string | null
+  filename: string | null
+}>({
+  link: null,
+  filename: null
+})
+const linkRef = ref()
+export function useFileSaver(linkText?: string) {
+  function FileSaver() {
+    return h(
+      'a',
+      {
+        href: linkAttrs.link,
+        download: linkAttrs.filename,
+        class: 'hidden',
+        ref: linkRef
+      },
+      [linkText ?? 'download link']
+    )
+  }
+  async function saveAs(objectUrl?: string, name?: string) {
+    if (!objectUrl || !name) return
+    linkAttrs.filename = name
+    linkAttrs.link = objectUrl
+
+    await nextTick()
+    if (linkRef.value) {
+      linkRef.value.click()
+    }
+  }
+  return {
+    FileSaver,
+    saveAs
+  }
+}
---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { fileURLToPath, URL } from 'node:url'
+import dts from 'vite-plugin-dts'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'node:path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), dts()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'vueFileSaver',
+      fileName: 'vue-file-saver'
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  }
+})
